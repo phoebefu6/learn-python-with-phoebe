c3759721990fc4c1a2b0bb83ffb2b799a137763e
Phoebe Fu <phoebe.fu66@gmail.com>
2026-06-23 13:34:12 +0800

    variables

--- projects/day01.py
+++ projects/day01.py
@@ -1,3 +1,30 @@
 print("hello yuan")
 print("hello world")
 print("")
+
+#print
+# print
+
+x = 1
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+level = 1
+print("you are at level", level)
+level = 2
+print("you are at level", level)
+
+print(level)
+
+a = 8
+b = a
+a = 2
+print(a)
+print(b)
+
+
+
+
